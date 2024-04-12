0009840055057be6d9eac52aff2f0900c51b350d
alberto <alberto.ortizdemendivil@ikasle.egibide.org>
2024-04-12 21:38:44 +0200

    nueva sol

--- pruebaGitJava/src/Main.java
+++ pruebaGitJava/src/Main.java
@@ -1,7 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int solucion;
+        solucion = Integer.parseInt(br.readLine());
+        if (solucion > 0){
+            System.out.println("mensaje alerta solucionado.");
+        }
     }
 }
